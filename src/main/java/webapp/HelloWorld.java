d9ea8de7aca45eaedd61140cea191a1f2ccf3a68
Endre Janák <janak.endre@gmail.com>
2016-10-11 13:15:06 +0200

    alap servlet keszitese web.xml segitsegevel

--- src/main/java/webapp/HelloWorld.java
+++ src/main/java/webapp/HelloWorld.java
@@ -11,9 +11,7 @@ public class HelloWorld extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		resp.getWriter().append("csako Világ");
 	}
 
-	
 }
